52ef68a184d21b4cb9607a9eaf7cd61dc7688118
José Carlos <josecarlos@globtec.com.br>
2018-01-03 09:28:07 -0200

    Adds create adr command

---
+++ src/Command/CreateAdrCommand.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace ADR\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class CreateAdrCommand extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('adr:create')
+            ->setDescription('Creates a new adr file')
+            ->setHelp('This command allows you to create a new adr file');
+    }
+}
---
+++ tests/Command/CreateAdrCommandTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace ADR\Command;
+
+use PHPUnit\Framework\TestCase;
+
+class CreateAdrCommandTest extends TestCase
+{
+    private $command;
+
+    public function setUp()
+    {
+        $this->command = new CreateAdrCommand();
+    }
+
+    public function testInstanceOfCreateAdrCommand()
+    {
+        $this->assertInstanceOf(CreateAdrCommand::class, $this->command);
+    }
+
+    public function testName()
+    {
+        $this->assertEquals('adr:create', $this->command->getName());
+    }
+
+    public function testDescription()
+    {
+        $this->assertEquals('Creates a new adr file', $this->command->getDescription());
+    }
+
+    public function testHelp()
+    {
+        $this->assertEquals('This command allows you to create a new adr file', $this->command->getHelp());
+    }
+}
