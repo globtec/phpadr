dc6c5897a849fc0009136834462d8b15b26e4790
José Carlos <josecarlos@globtec.com.br>
2018-01-04 09:21:11 -0200

    Adds the argument title for the command

--- src/Command/CreateAdrCommand.php
+++ src/Command/CreateAdrCommand.php
@@ -3,16 +3,21 @@
 namespace ADR\Command;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class CreateAdrCommand extends Command
 {
+    /**
+     * Configures the command
+     */
     protected function configure()
     {
         $this
             ->setName('adr:create')
             ->setDescription('Creates a new ADR file')
-            ->setHelp('This command allows you to create a new ADR file');
+            ->setHelp('This command allows you to create a new ADR file')
+            ->addArgument('title', InputArgument::REQUIRED, 'The title of the ADR');
     }
 }
